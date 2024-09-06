04a0bbd4347067c9ee94b99e5151987a61d9eac6
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-09-06 09:21:08 -0700

    add ShiftRows transformation

--- aes/aes.go
+++ aes/aes.go
@@ -2,6 +2,7 @@ package aes
 
 
 func generate_round_keys(secret_key int) {
+
 }
 
 
@@ -15,21 +16,57 @@ func subBytes() {
 }
 
 
+func ShiftRows(state [16]byte) [16]byte {
+
+	// 0 1 2 3
+	// 4 5 6 7
+	// 8 9 0 1
+	// 2 3 4 5
+	var temp [3]byte
+
+	temp[0] = state[4]
+	state[4] = state[5]
+	state[5] = state[6]
+	state[6] = state[7]
+	state[7] = temp[0]
+
+	temp[0] = state[8]
+	temp[1] = state[9]
+	state[8] = state[10]
+	state[9] = state[11]
+	state[10] = temp[0]
+	state[11] = temp[1]
+
+	temp[0] = state[12]
+	temp[1] = state[13]
+	temp[2] = state[14]
+	state[12] = state[15]
+	state[13] = temp[0]
+	state[14] = temp[1]
+	state[15] = temp[2]
+
+	return state
+}
+
+func mixColumns() {
+
+}
+
+func addKey() {
+
+}
 
 
-
-func aes_encrypt (block [16]byte, key string) {
+func aesEncrypt (state [16]byte, key string) {
 
 	// First get round keys
 
 
 	// Add round keys
 
-	// 
+}
 
 
-
-
-
-
+func gfAdd(a, b byte) {
+	
 }
--- main.go
+++ main.go
@@ -8,4 +8,14 @@ import (
 func main() {
 
   fmt.Println("Ahmad")
+
+  state := [16]byte {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+
+  state = aes.ShiftRows(state)
+
+  fmt.Println(state[:4])
+  fmt.Println(state[4:8])
+  fmt.Println(state[8:12])
+  fmt.Println(state[12:])
+
 }
