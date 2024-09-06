2b4a19091af1ccb9984c6feeec9105a8850cbdb4
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-09-06 09:49:40 -0700

    implement SubBytes transformation

--- aes/aes.go
+++ aes/aes.go
@@ -11,35 +11,35 @@ func generate_round_keys(secret_key int) {
 // Transformations
 
 
-func subBytes() {
+func SubBytes(state [16]byte) [16]byte {
 
+	for ix, value := range state {
+		state[ix] = sbox0[value]
+	}
+	return state
 }
 
 
 func ShiftRows(state [16]byte) [16]byte {
 
-	// 0 1 2 3
-	// 4 5 6 7
-	// 8 9 0 1
-	// 2 3 4 5
 	var temp [3]byte
 
-	temp[0] = state[4]
-	state[4] = state[5]
-	state[5] = state[6]
-	state[6] = state[7]
-	state[7] = temp[0]
+	temp[0]   = state[4]
+	state[4]  = state[5]
+	state[5]  = state[6]
+	state[6]  = state[7]
+	state[7]  = temp[0]
 
-	temp[0] = state[8]
-	temp[1] = state[9]
-	state[8] = state[10]
-	state[9] = state[11]
+	temp[0]   = state[8]
+	temp[1]   = state[9]
+	state[8]  = state[10]
+	state[9]  = state[11]
 	state[10] = temp[0]
 	state[11] = temp[1]
 
-	temp[0] = state[12]
-	temp[1] = state[13]
-	temp[2] = state[14]
+	temp[0]   = state[12]
+	temp[1]   = state[13]
+	temp[2]   = state[14]
 	state[12] = state[15]
 	state[13] = temp[0]
 	state[14] = temp[1]
--- main.go
+++ main.go
@@ -7,15 +7,39 @@ import (
 
 func main() {
 
-  fmt.Println("Ahmad")
+  state := [16]byte {0x74, 0xc5, 0xdf, 0x3c, 
+                      0x6c, 0x1e,  0x93, 0x62,
+                      0xe1, 0xdd, 0x79, 0xb0,
+                      0x09, 0x3b, 0xc7, 0xe7}
 
-  state := [16]byte {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+  // state := [16]byte {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
 
-  state = aes.ShiftRows(state)
+  state = aes.SubBytes(state)
+  // state = aes.ShiftRows(state)
 
-  fmt.Println(state[:4])
-  fmt.Println(state[4:8])
-  fmt.Println(state[8:12])
-  fmt.Println(state[12:])
+  // fmt.Println(state[:4])
+  // fmt.Println(state[4:8])
+  // fmt.Println(state[8:12])
+  // fmt.Println(state[12:])
+
+  for _, b := range state[:4] {
+    fmt.Printf("%02x ", b)
+  }
+  fmt.Println()
+
+  for _, b := range state[4:8] {
+    fmt.Printf("%02x ", b)
+  }
+  fmt.Println()
+
+  for _, b := range state[8:12] {
+    fmt.Printf("%02x ", b)
+  }
+  fmt.Println()
+
+  for _, b := range state[12:] {
+    fmt.Printf("%02x ", b)
+  }
+  fmt.Println()
 
 }
