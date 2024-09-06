3d86bb5a3e5f55c91b6a225c8a3d7c3453630583
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-09-05 22:39:14 -0700

    add aes directory

---
+++ AES/aes.go
@@ -0,0 +1,17 @@
+package aes;
+
+func aes_encrypt (block [16]byte, key string) {
+
+	// First get round keys
+
+
+	// Add round keys
+
+	// 
+
+
+
+
+
+
+}
---
+++ AES/go.mod
@@ -0,0 +1,3 @@
+module github.com/ADHFMZ7/cryptos/aes
+
+go 1.21.6
