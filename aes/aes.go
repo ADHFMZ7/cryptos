d33b68f2e8b398efe941147d3321121bed2c5f62
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-09-06 19:08:44 -0700

    switch to column order for shift and add PrintState

--- aes/aes.go
+++ aes/aes.go
@@ -1,14 +1,27 @@
 package aes
 
+import "fmt"
 
 func generate_round_keys(secret_key int) {
 
 }
 
+//  0 4 8  12
+//  1 5 9  13
+//  2 6 10 14
+//  3 7 11 15
 
-
-
-// Transformations
+func PrintState(state [16]byte) {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			// Calculate the index for the state matrix
+			index := i + 4*j
+			// Print each byte in hex format
+			fmt.Printf("%02x ", state[index])
+		}
+		fmt.Println() // New line after each row
+	}
+}
 
 func SubBytes(state [16]byte) [16]byte {
 
@@ -22,25 +35,25 @@ func ShiftRows(state [16]byte) [16]byte {
 
 	var temp [3]byte
 
-	temp[0]   = state[4]
-	state[4]  = state[5]
-	state[5]  = state[6]
-	state[6]  = state[7]
-	state[7]  = temp[0]
+	temp[0]   = state[1]
+	state[1]  = state[5]
+	state[5]  = state[9]
+	state[9]  = state[13]
+	state[13]  = temp[0]
 
-	temp[0]   = state[8]
-	temp[1]   = state[9]
-	state[8]  = state[10]
-	state[9]  = state[11]
+	temp[0]   = state[2]
+	temp[1]   = state[6]
+	state[2]  = state[10]
+	state[6]  = state[14]
 	state[10] = temp[0]
-	state[11] = temp[1]
-
-	temp[0]   = state[12]
-	temp[1]   = state[13]
-	temp[2]   = state[14]
-	state[12] = state[15]
-	state[13] = temp[0]
 	state[14] = temp[1]
+
+	temp[0]   = state[3]
+	temp[1]   = state[7]
+	temp[2]   = state[11]
+	state[3] = state[15]
+	state[7] = temp[0]
+	state[11] = temp[1]
 	state[15] = temp[2]
 
 	return state
@@ -65,23 +78,64 @@ func MixColumn(c0, c1, c2, c3 *byte) {
 func MixColumns(state [16]byte) [16] byte {
 
 	for ix := 0; ix < 4; ix++ {
-		MixColumn(&state[0 + ix], &state[4 + ix], &state[8 + ix], &state[12 + ix])
+		MixColumn(&state[0 + ix], &state[1 + ix], &state[2 + ix], &state[3 + ix])
 	}
 
 	return state
+}
+
+func RotWord(column [4]byte) [4]byte {
+
+	var temp byte
+
+	temp = column[0]
+	column[0] = column[1]
+	column[1] = column[2]
+	column[2] = column[3]
+	column[3] = temp
+
+	return column
 }
 
 func addKey() {
 
 }
 
+func AesKeySchedule(secretKey [16]byte) {
 
-func aesEncrypt (state [16]byte, key string) {
+	var keys_out [11][16]byte
 
-	// First get round keys
+	keys_out[0] = secretKey
 
 
-	// Add round keys
+	for ix := 1; ix <= 11; ix++ {
+		
+		
+
+
+	}
+
+
+
+}
+
+func aesEncrypt (state, keySchedule [16]byte) {
+
+
+
+
+	for ix := 0; ix < 9; ix++ {
+
+		state = SubBytes(state)
+		state = ShiftRows(state)
+		state = MixColumns(state)
+		// state = AddKey(state, key)
+
+	}
+
+	state = SubBytes(state)
+	state = ShiftRows(state)
+	// state = AddKey(state, key)
 
 }
 
