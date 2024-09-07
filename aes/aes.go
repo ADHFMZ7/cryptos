f85f7467818bbf5565ffd7a0d7bdb1fe914d5e55
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-09-07 13:52:02 -0700

    decryption working

--- aes/aes.go
+++ aes/aes.go
@@ -26,6 +26,14 @@ func subBytes(state [16]byte) [16]byte {
 	return state
 }
 
+func subBytesInv(state [16]byte) [16]byte {
+	for ix, value := range state {
+		state[ix] = sboxinv[value]
+	}
+	return state
+
+}
+
 func shiftRows(state [16]byte) [16]byte {
 
 	var temp [3]byte
@@ -54,6 +62,34 @@ func shiftRows(state [16]byte) [16]byte {
 	return state
 }
 
+func shiftRowsInv(state [16]byte) [16]byte {
+
+	var temp [3]byte
+
+	temp[0] = state[13]
+	state[13] = state[9]
+	state[9] = state[5]
+	state[5] = state[1]
+	state[1] = temp[0]
+
+	temp[0] = state[14]
+	temp[1] = state[10]
+	state[14] = state[6]
+	state[10] = state[2]
+	state[6] = temp[0]
+	state[2] = temp[1]
+
+	temp[0] = state[15]
+	temp[1] = state[11]
+	temp[2] = state[7]
+	state[15] = state[3]
+	state[11] = temp[0]
+	state[7] = temp[1]
+	state[3] = temp[2]
+
+	return state
+}
+
 func mixColumn(column [4]*byte) {
 	temp := [4]byte{0, 0, 0, 0}
 
@@ -75,6 +111,30 @@ func mixColumns(state [16]byte) [16]byte {
 	}
 
 	return state
+}
+
+func mixColumnsInv(state [16]byte) [16]byte {
+
+	for ix := 0; ix < 4; ix++ {
+		mixColumnInv([4]*byte{&state[ix*4], &state[ix*4+1], &state[ix*4+2], &state[ix*4+3]})
+	}
+
+	return state
+}
+
+func mixColumnInv(column [4]*byte) {
+
+	temp := [4]byte{0, 0, 0, 0}
+
+	temp[0] = GFMul(0x0E, *column[0]) ^ GFMul(0x0B, *column[1]) ^ GFMul(0x0D, *column[2]) ^ GFMul(0x09, *column[3])
+	temp[1] = GFMul(0x09, *column[0]) ^ GFMul(0x0E, *column[1]) ^ GFMul(0x0B, *column[2]) ^ GFMul(0x0D, *column[3])
+	temp[2] = GFMul(0x0D, *column[0]) ^ GFMul(0x09, *column[1]) ^ GFMul(0x0E, *column[2]) ^ GFMul(0x0B, *column[3])
+	temp[3] = GFMul(0x0B, *column[0]) ^ GFMul(0x0D, *column[1]) ^ GFMul(0x09, *column[2]) ^ GFMul(0x0E, *column[3])
+
+	*column[0] = temp[0]
+	*column[1] = temp[1]
+	*column[2] = temp[2]
+	*column[3] = temp[3]
 }
 
 func rotWord(column [4]byte) [4]byte {
@@ -147,7 +207,9 @@ func KeySchedule(secretKey [16]byte) [11][16]byte {
 	return keys_out
 }
 
-func Encrypt(state [16]byte, keySchedule [11][16]byte) [16]byte {
+func Encrypt(state [16]byte, privateKey [16]byte) [16]byte {
+
+	keySchedule := KeySchedule(privateKey)
 
 	state = addKey(state, keySchedule[0])
 
@@ -165,6 +227,25 @@ func Encrypt(state [16]byte, keySchedule [11][16]byte) [16]byte {
 	state = addKey(state, keySchedule[10])
 
 	return state
+}
+
+func Decrypt(ciphertext [16]byte, privateKey [16]byte) [16]byte {
+
+	keySchedule := KeySchedule(privateKey)
+
+	ciphertext = addKey(ciphertext, keySchedule[10])
+	ciphertext = shiftRowsInv(ciphertext)
+	ciphertext = subBytesInv(ciphertext)
+
+	for ix := 9; ix > 0; ix-- {
+		ciphertext = addKey(ciphertext, keySchedule[ix])
+		ciphertext = mixColumnsInv(ciphertext)
+		ciphertext = shiftRowsInv(ciphertext)
+		ciphertext = subBytesInv(ciphertext)
+	}
+
+	ciphertext = addKey(ciphertext, keySchedule[0])
+	return ciphertext
 }
 
 func wordAdd(a [4]*byte, b [4]byte) {
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 
+	fmt.Println("AES128 Encryption")
+
 	key := [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 
 	in := [16]byte{
@@ -15,15 +17,15 @@ func main() {
 		0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
 	}
 
-	schedule := aes.KeySchedule(key)
+	aes.PrintState(in)
+	fmt.Println()
 
-	for ix := range schedule {
-		fmt.Println("\n ", ix)
-		aes.PrintState(schedule[ix])
-		fmt.Println("\n ")
-	}
-
-	ciphertext := aes.Encrypt(in, schedule)
+	ciphertext := aes.Encrypt(in, key)
 
 	aes.PrintState(ciphertext)
+	fmt.Println()
+
+	encrypted := aes.Decrypt(ciphertext, key)
+	aes.PrintState(encrypted)
+
 }
